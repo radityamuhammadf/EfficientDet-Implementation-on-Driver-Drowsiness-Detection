dee2ee49703d5daf233a28c2c8188d0096ad83e0
118140129Arre <80655807+118140129Arre@users.noreply.github.com>
2024-06-07 16:04:35 +0700

    separate live detection and video input

--- video_input.py
+++ video_input.py
@@ -6,15 +6,13 @@ from torch.backends import cudnn
 from backbone import EfficientDetBackbone
 from efficientdet.utils import BBoxTransform, ClipBoxes
 from utils.utils import preprocess, invert_affine, postprocess, preprocess_video
-from torch.profiler import profile, record_function, ProfilerActivity
 import os
-
 
 def main():
     current_directory = os.getcwd()
     # Video's path
-    video_src=os.path.join(current_directory, r"test_video\10-MaleGlasses-Trim.avi")
-    # video_src = 0  # set int to use webcam, set str to read from a video file
+    # video_src=os.path.join(current_directory, r"test_video\10-MaleGlasses-Trim.avi")
+    video_src = 0  # set int to use webcam, set str to read from a video file
 
     compound_coef = 0
     force_input_size = None  # set None to use default size
@@ -27,7 +25,6 @@ def main():
     cudnn.fastest = True
     cudnn.benchmark = True
     obj_list = ['closed-eyes', 'yawn']
-
 
     # Initialize the dictionary to keep track of detection times and last durations
     detections = {
@@ -58,16 +55,44 @@ def main():
     regressBoxes = BBoxTransform()
     clipBoxes = ClipBoxes()
 
-    # Video capture
-    cap = cv2.VideoCapture(video_src)
-    # Frame per Second variable initiation
-    fps=cap.get(cv2.CAP_PROP_FPS)
-    frame_duration=1/fps
-    frame_number=0
-    print("FPS: ",fps)
 
+    #creating videos_metadata dictionary
+    videos_metadata = {
+        'webcam': {
+            'path': 0,
+            'light_sufficient': True,
+            'looking_lr': False,
+            'detected_drowsiness': [],
+            'ground_truth_drowsiness': []
+        },
+        'debug_video_sample': {
+            'path': os.path.join(current_directory, 'test_video\debugging_sample.avi'),
+            'light_sufficient': True,
+            'looking_lr': False,
+            'detected_drowsiness': [],
+            'ground_truth_drowsiness': []
+        }
+    }
+    # #example of adding metadata (used later for the four video data)
+    # videos_metadata[video_name] = {
+    #     'path': os.path.join(current_directory, 'path/to/another/vid'),
+    #     'light_sufficient': False,
+    #     'looking_lr': True,
+    #     'detected_drowsiness': [0.1, 0.3, 0.5],  # Example list of floats
+    #     'ground_truth_drowsiness': [0.2, 0.4, 0.6]  # Example list of floats
+    # }
 
-    with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    #iterating every metadata element in every 'video_name' (videos_metadata members) element
+    for video_name, metadata in videos_metadata.items():
+        video_path = metadata['path']
+        # Start the webcam
+        cap = cv2.VideoCapture(video_path)
+        # cap = cv2.VideoCapture(os.path.join(current_directory, r"test_video\10-MaleGlasses-Trim.avi"))  
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        frame_duration = 1 / fps
+        frame_number = 0
+        print("FPS: ", fps)
+
         while True:
             ret, frame = cap.read()
             if not ret:
@@ -84,70 +109,64 @@ def main():
             x = x.to(torch.float32 if not use_float16 else torch.float16).permute(0, 3, 1, 2)
 
             with torch.no_grad():
-                with record_function("model_inference"):
-                    features, regression, classification, anchors = model(x)
+                features, regression, classification, anchors = model(x)
+                out = postprocess(x, anchors, regression, classification, regressBoxes, clipBoxes, threshold,
+                                iou_threshold)
 
-                with record_function("postprocess"):
-                    out = postprocess(x, anchors, regression, classification, regressBoxes, clipBoxes, threshold,
-                                      iou_threshold)
+            out = invert_affine(framed_metas, out)
 
-                out = invert_affine(framed_metas, out)
+            current_detections = set()
+            for i in range(len(ori_imgs)):
+                for j in range(len(out[i]['rois'])):
+                    (x1, y1, x2, y2) = out[i]['rois'][j].astype(int)
+                    cv2.rectangle(ori_imgs[i], (x1, y1), (x2, y2), (255, 255, 0), 2)
+                    obj = obj_list[out[i]['class_ids'][j]]
+                    score = float(out[i]['scores'][j])
 
-                current_detections = set()
-                for i in range(len(ori_imgs)):
-                    for j in range(len(out[i]['rois'])):
-                        (x1, y1, x2, y2) = out[i]['rois'][j].astype(int)
-                        cv2.rectangle(ori_imgs[i], (x1, y1), (x2, y2), (255, 255, 0), 2)
-                        obj = obj_list[out[i]['class_ids'][j]]
-                        score = float(out[i]['scores'][j])
+                    cv2.putText(ori_imgs[i], '{}, {:.3f}'.format(obj, score),
+                                (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                                (255, 255, 0), 1)
 
-                        cv2.putText(ori_imgs[i], '{}, {:.3f}'.format(obj, score),
-                                    (x1, y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                    (255, 255, 0), 1)
+                    # recording the frame count
+                    class_name = obj  # reusing the obj variable to match other repo's paradigm
+                    if detections[class_name]['last_seen_frame'] is None:
+                        detections[class_name]['last_seen_frame'] = frame_number
+                    else:
+                        detections[class_name]['frame_count'] += frame_number - detections[class_name][
+                            'last_seen_frame']
+                        detections[class_name]['last_seen_frame'] = frame_number
+                    detections[class_name]['was_detected'] = True  # params to state the detected state
 
-                        # recording the frame count
-                        class_name = obj  # reusing the obj variable to match other repo's paradigm
-                        if detections[class_name]['last_seen_frame'] is None:
-                            detections[class_name]['last_seen_frame'] = frame_number
-                        else:
-                            detections[class_name]['frame_count'] += frame_number - detections[class_name][
-                                'last_seen_frame']
-                            detections[class_name]['last_seen_frame'] = frame_number
-                        detections[class_name]['was_detected'] = True  # params to state the detected state
+                    current_detections.add(class_name)
 
-                        current_detections.add(class_name)
+            # Reset the durations when the class are not detected anymore
+            for class_name in detections:
+                if class_name not in current_detections:  # check whether this current frame still detecting the same class
+                    detections[class_name]['was_detected'] = False  # reset the previously detected class state
+                    detections[class_name]['frame_count'] = 0  # reset counted frame value
+                    detections[class_name]['last_seen_frame'] = None  # reset previously seen frame
 
-                # Reset the durations when the class are not detected anymore
-                for class_name in detections:
-                    if class_name not in current_detections:  # check whether this current frame still detecting the same class
-                        detections[class_name]['was_detected'] = False  # reset the previously detected class state
-                        detections[class_name]['frame_count'] = 0  # reset counted frame value
-                        detections[class_name]['last_seen_frame'] = None  # reset previously seen frame
+            # Convert frame counts to time using FPS
+            for class_name in detections:
+                # this default frames per second are 30FPS -> and the duration for each frame are 1/30 ~ 0.33
+                # for instance, if the closed-eyes class are detected for 65 frames consecutively
+                # that means the durations of detected closed-eyes are 65*0.03 which are 1.95s
+                detections[class_name]['duration'] = detections[class_name]['frame_count'] * frame_duration
 
-                # Convert frame counts to time using FPS
-                for class_name in detections:
-                    #this default frames per second are 30FPS -> and the duration for each frame are 1/30 ~ 0.33
-                    #for instance, if the closed-eyes class are detected for 65 frames consecutively
-                    #that means the durations of detected closed-eyes are 65*0.03 which are 1.95s
-                    detections[class_name]['duration'] = detections[class_name]['frame_count'] * frame_duration
+            # Detect drowsiness based on the duration of closed-eyes and yawn
+            closed_eyes_duration = detections['closed-eyes']['duration']
+            yawn_duration = detections['yawn']['duration']
 
-                # Detect drowsiness based on the duration of closed-eyes and yawn
-                closed_eyes_duration = detections['closed-eyes']['duration']
-                yawn_duration = detections['yawn']['duration']
+            # Logic for detecting drowsiness
+            if closed_eyes_duration > 0.5 or yawn_duration > 5.0:  # thresholds in seconds
+                drowsy_state = True
+            else:
+                drowsy_state = False
 
-                # Logic for detecting drowsiness
-                if closed_eyes_duration > 0.5 or yawn_duration > 5.0:  # thresholds in seconds
-                    drowsy_state = True
-                else:
-                    drowsy_state = False
-
-                #Drowsy State Branch Logic
-                if drowsy_state is True:
-                    #                     x1, y1     x2,  y2    r,    g ,  b    (lupa ini apa)
-                    cv2.rectangle(frame, (500, 20), (640, 60), (255, 255, 255), -1)
-                    cv2.putText(frame, 'Drowsy', (500, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
-                    #continue adding logic regarding drowsy state here
-                    #drowsy counter (?)
+            # Drowsy State Branch Logic
+            if drowsy_state is True:
+                cv2.rectangle(frame, (500, 20), (640, 60), (255, 255, 255), -1)
+                cv2.putText(frame, 'Drowsy', (500, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
 
             # debugging print state
             print(f"Closed-eyes duration: {closed_eyes_duration:.2f} seconds")
@@ -158,12 +177,10 @@ def main():
             if drowsy_state is True:
                 pass
 
-
-
             """
             Static Information Display Function
             """
-            #drawing and writing the annotation
+            # drawing and writing the annotation
             cv2.rectangle(frame, (0, 10), (200, 60), (255, 255, 255), -1)
             y_offset = 30
             cv2.putText(frame, f'closed-eyes: {closed_eyes_duration:.2f} s', (10, y_offset), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 2)
@@ -175,10 +192,8 @@ def main():
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-    cap.release()
+        cap.release()
     cv2.destroyAllWindows()
 
 if __name__=='__main__':
     main()
-# Print profiling results
-# print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
